af8c2f49f0da63e0f931651304bd316b69a42549
Leonardo Bonfim <leonardolbs_bonfim@gmail.com>
2019-08-20 02:51:05 -0300

    Criado Pagina Principal e definido o layout do header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'principal', loadChildren: './pages/principal/principal.module#PrincipalPageModule' },
 ];
 
 @NgModule({
