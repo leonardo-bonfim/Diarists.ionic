f3cf2a0ad389dafc80c0d5ffcd3a0bed64a298c4
Leonardo Bonfim <leonardolbs_bonfim@gmail.com>
2019-09-10 22:39:21 -0300

    Adicionado BrMask para o CPF no Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Camera } from '@ionic-native/Camera/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { SegurancaModule } from './seguranca/seguranca.module';
 import { ApiRequestService } from './services/api-request.service';
 import { AlertService } from './services/alert.service';
--- src/app/pages/cadastro/cadastro.module.ts
+++ src/app/pages/cadastro/cadastro.module.ts
@@ -1,3 +1,4 @@
+import { BrMaskerModule } from 'br-mask';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,6 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
+    BrMaskerModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
